4f90221f4062d34f663331dc55a1694dcc726a96
duong_nguyen <ntduong411@gmail.com>
2023-09-29 15:41:39 +0700

    hello

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ POS_LV_3 = 32
 POS_LV_4 = 38
 POS_LV_5 = 44
 POS_LV_6 = 50
-POS_LV_7 = 70
-POS_LV_8 = 90
+POS_LV_7 = 60
+POS_LV_8 = 70
 POS_LV_9 = 115
 POS_LV_10 = 116
 
 NEG_LV_1 = -120
-NEG_LV_2 = -74
-NEG_LV_3 = -54
-NEG_LV_4 = -34
-NEG_LV_5 = -28
-NEG_LV_6 = -22
-NEG_LV_7 = -16
-NEG_LV_8 = -10
-NEG_LV_9 = -4
+NEG_LV_2 = -56
+NEG_LV_3 = -46
+NEG_LV_4 = -35
+NEG_LV_5 = -29
+NEG_LV_6 = -23
+NEG_LV_7 = -17
+NEG_LV_8 = -11
+NEG_LV_9 = -5
 
 SUN = 0
 MOON = 1
@@ -89,119 +89,128 @@ def score_houses(first, second):
 
     return score
 
-def cal_matching_percent(pos_score, neg_score):
+def cal_matching_percent(num_score_types):
+    pos_score = num_score_types[1] + num_score_types[2] * 2 + num_score_types[3] * 3 + num_score_types[4] * 4 + score_houses(first, second)
+    neg_score = - (num_score_types[-1] + num_score_types[-2] * 2 + num_score_types[-3] * 3 + num_score_types[-4] * 4)
+
     result = 0
     if   pos_score <= POS_LV_1  and neg_score <= NEG_LV_1:     result = 1
-    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_2:     result = 3
-    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_3:     result = 8
-    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_4:     result = 12
-    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_5:     result = 22
-    elif pos_score <= POS_LV_1  and neg_score >  NEG_LV_6:     result = 35
-    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_7:     result = 47
-    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_8:     result = 56
-    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_9:     result = 59
-    elif pos_score <= POS_LV_1  and neg_score >  NEG_LV_9:     result = 61
+    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_2:     result = 2
+    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_3:     result = 18
+    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_4:     result = 19
+    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_5:     result = 23
+    elif pos_score <= POS_LV_1  and neg_score >  NEG_LV_6:     result = 32
+    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_7:     result = 34
+    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_8:     result = 40
+    elif pos_score <= POS_LV_1  and neg_score <= NEG_LV_9:     result = 41
+    elif pos_score <= POS_LV_1  and neg_score >  NEG_LV_9:     result = 44
    
-    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_1:     result = 2
-    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_2:     result = 5
-    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_3:     result = 10
-    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_4:     result = 15
-    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_5:     result = 24
-    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_6:     result = 37
-    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_7:     result = 51
-    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_8:     result = 58
-    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_9:     result = 62
-    elif pos_score <= POS_LV_2  and neg_score >  NEG_LV_9:     result = 65
+    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_1:     result = 3
+    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_2:     result = 4
+    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_3:     result = 20
+    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_4:     result = 24
+    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_5:     result = 33
+    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_6:     result = 36
+    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_7:     result = 38
+    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_8:     result = 47
+    elif pos_score <= POS_LV_2  and neg_score <= NEG_LV_9:     result = 50
+    elif pos_score <= POS_LV_2  and neg_score >  NEG_LV_9:     result = 61
 
-    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_1:     result = 4
+    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_1:     result = 5
     elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_2:     result = 7
-    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_3:     result = 13
-    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_4:     result = 18
-    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_5:     result = 28
-    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_6:     result = 40
-    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_7:     result = 55
-    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_8:     result = 63
-    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_9:     result = 68
-    elif pos_score <= POS_LV_3  and neg_score >  NEG_LV_9:     result = 71
+    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_3:     result = 21
+    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_4:     result = 26
+    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_5:     result = 35
+    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_6:     result = 39
+    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_7:     result = 51
+    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_8:     result = 54
+    elif pos_score <= POS_LV_3  and neg_score <= NEG_LV_9:     result = 60
+    elif pos_score <= POS_LV_3  and neg_score >  NEG_LV_9:     result = 65
 
     elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_1:     result = 6
-    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_2:     result = 11
-    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_3:     result = 17
-    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_4:     result = 23
-    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_5:     result = 30
+    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_2:     result = 8
+    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_3:     result = 22
+    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_4:     result = 27
+    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_5:     result = 37
     elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_6:     result = 43
-    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_7:     result = 57
-    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_8:     result = 66
-    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_9:     result = 70
-    elif pos_score <= POS_LV_4  and neg_score >  NEG_LV_9:     result = 76
+    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_7:     result = 59
+    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_8:     result = 63
+    elif pos_score <= POS_LV_4  and neg_score <= NEG_LV_9:     result = 66
+    elif pos_score <= POS_LV_4  and neg_score >  NEG_LV_9:     result = 74
 
     elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_1:     result = 9
-    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_2:     result = 16
-    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_3:     result = 20
-    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_4:     result = 27
-    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_5:     result = 34  
-    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_6:     result = 45
-    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_7:     result = 64
-    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_8:     result = 73
+    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_2:     result = 10
+    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_3:     result = 25
+    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_4:     result = 42
+    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_5:     result = 46  
+    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_6:     result = 53
+    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_7:     result = 62
+    elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_8:     result = 68
     elif pos_score <= POS_LV_5  and neg_score <= NEG_LV_9:     result = 77
     elif pos_score <= POS_LV_5  and neg_score >  NEG_LV_9:     result = 80
 
-    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_1:     result = 14
-    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_2:     result = 21
-    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_3:     result = 25
-    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_4:     result = 31
-    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_5:     result = 39
-    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_6:     result = 49
-    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_7:     result = 69
-    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_8:     result = 75
-    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_9:     result = 79
-    elif pos_score <= POS_LV_6  and neg_score >  NEG_LV_9:     result = 85
+    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_1:     result = 11
+    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_2:     result = 12
+    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_3:     result = 28
+    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_4:     result = 48
+    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_5:     result = 52
+    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_6:     result = 58
+    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_7:     result = 71
+    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_8:     result = 76
+    elif pos_score <= POS_LV_6  and neg_score <= NEG_LV_9:     result = 82
+    elif pos_score <= POS_LV_6  and neg_score >  NEG_LV_9:     result = 87
 
-    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_1:     result = 19
-    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_2:     result = 29
-    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_3:     result = 33
-    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_4:     result = 38
-    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_5:     result = 48
-    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_6:     result = 67
-    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_7:     result = 74
-    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_8:     result = 84
-    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_9:     result = 86
-    elif pos_score <= POS_LV_7  and neg_score >  NEG_LV_9:     result = 91
+    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_1:     result = 13
+    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_2:     result = 14
+    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_3:     result = 31
+    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_4:     result = 49
+    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_5:     result = 67
+    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_6:     result = 73
+    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_7:     result = 75
+    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_8:     result = 85
+    elif pos_score <= POS_LV_7  and neg_score <= NEG_LV_9:     result = 89
+    elif pos_score <= POS_LV_7  and neg_score >  NEG_LV_9:     result = 92
 
-    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_1:     result = 26
-    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_2:     result = 36
-    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_3:     result = 42
-    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_4:     result = 46
-    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_5:     result = 54
+    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_1:     result = 15
+    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_2:     result = 16
+    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_3:     result = 45
+    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_4:     result = 56
+    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_5:     result = 72
     elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_6:     result = 78
-    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_7:     result = 83
+    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_7:     result = 84
     elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_8:     result = 90
-    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_9:     result = 93
-    elif pos_score <= POS_LV_8  and neg_score >  NEG_LV_9:     result = 97
+    elif pos_score <= POS_LV_8  and neg_score <= NEG_LV_9:     result = 94
+    elif pos_score <= POS_LV_8  and neg_score >  NEG_LV_9:     result = 96
 
-    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_1:     result = 32
-    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_2:     result = 44
-    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_3:     result = 50
-    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_4:     result = 52
-    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_5:     result = 72
-    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_6:     result = 82
+    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_1:     result = 17
+    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_2:     result = 29
+    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_3:     result = 57
+    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_4:     result = 69
+    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_5:     result = 79
+    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_6:     result = 81
     elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_7:     result = 88
-    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_8:     result = 92
-    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_9:     result = 95
-    elif pos_score <= POS_LV_9  and neg_score >  NEG_LV_9:     result = 98
+    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_8:     result = 93
+    elif pos_score <= POS_LV_9  and neg_score <= NEG_LV_9:     result = 97
+    elif pos_score <= POS_LV_9  and neg_score >  NEG_LV_9:     result = 99
 
-    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_1:     result = 41
-    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_2:     result = 53
-    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_3:     result = 60
-    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_4:     result = 81
-    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_5:     result = 87
-    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_6:     result = 89
-    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_7:     result = 94
-    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_8:     result = 96
-    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_9:     result = 99
+    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_1:     result = 30
+    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_2:     result = 55
+    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_3:     result = 64
+    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_4:     result = 70
+    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_5:     result = 83
+    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_6:     result = 86
+    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_7:     result = 91
+    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_8:     result = 95
+    elif pos_score > POS_LV_10  and neg_score <= NEG_LV_9:     result = 98
     elif pos_score > POS_LV_10  and neg_score >  NEG_LV_9:     result = 100
 
-    return result / 100
+    refined_result = result         \
+        + num_score_types[4] * 0    \
+        - num_score_types[-4] * 5   \
+        + num_score_types[3] * 0    \
+        - num_score_types[-3] * 0
+
+    return (pos_score, neg_score, round(result / 100, 2), round(refined_result / 100, 2))
 
 results = []
 
@@ -254,14 +263,15 @@ with open('samples.csv', mode='r') as csv_file:
                     #     pos_scores.append(rc["score"]) 
                     # if rc["score"] < 0:
                     #     neg_scores.append(rc["score"]) 
-        pos_sum = num_score_types[1] + num_score_types[2] * 2 + num_score_types[3] * 3 + num_score_types[4] * 4 + score_houses(first, second)
-        neg_sum = num_score_types[-1] + num_score_types[-2] * 2 + num_score_types[-3] * 3 + num_score_types[-4] * 4
+        
+        (pos_score, neg_score, matching_score, refined_matching_score) = cal_matching_percent(num_score_types)
+        
         results.append([
             first["name"], second["name"], 
-            pos_sum, -neg_sum,
+            pos_score, neg_score,
             num_score_types[1], num_score_types[2], num_score_types[3], num_score_types[4],
             num_score_types[-1], num_score_types[-2], num_score_types[-3], num_score_types[-4], 
-            cal_matching_percent(pos_sum, -neg_sum)])
+            matching_score, refined_matching_score])
     
 # Exort file
 with open('results.csv', mode='w', newline='') as results_file:
