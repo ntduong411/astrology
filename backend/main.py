65c4b063df4d859b9928178308f899ea6a126fa0
duong_nguyen <ntduong411@gmail.com>
2023-10-08 12:04:46 +0700

    Export reports

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from kerykeion import AstrologicalSubject, KerykeionChartSVG
-import csv
+import csv, json
 from pprint import pprint
 from utils import *
 from destiny import *
@@ -9,19 +9,20 @@ logging.getLogger().setLevel(logging.ERROR)
 
 results = []
 
-
 # Read samples
 samples = []
-with open('../database/samples.csv', mode='r') as csv_file:
+with open('../database/test_samples.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
         first = AstrologicalSubject(row["name_1"], int(row["year_1"]), int(row["month_1"]), int(row["day_1"]), int(row["hour_1"]), int(row["minute_1"]), row["place_1"])
         second = AstrologicalSubject(row["name_2"], int(row["year_2"]), int(row["month_2"]), int(row["day_2"]), int(row["hour_2"]), int(row["minute_2"]), row["place_2"])
         destiny = Destiny(first, second)
-        pprint (destiny.report())
-        exit()
+        results.append(destiny.report())
     
 # Exort file
-with open('results.csv', mode='w', newline='') as results_file:
-    writer = csv.writer(results_file, delimiter=',')
-    writer.writerows(results)
+json_results = json.dumps(results, indent=2)
+with open("../database/results.json", "w") as outfile:
+    outfile.write(json_results)
+# with open('results.csv', mode='w', newline='') as results_file:
+#     writer = csv.writer(results_file, delimiter=',')
+#     writer.writerows(results)
